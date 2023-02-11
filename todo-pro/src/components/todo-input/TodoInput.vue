d397c0ae5a89b0ec672fc3dbd4bd7cf2e9955c53
gangao123 <9400310+gangao123@user.noreply.gitee.com>
2023-02-11 15:49:17 +0800

    first commit

---
+++ todo-pro/src/components/todo-input/TodoInput.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <form class="mt-4" @submit.prevent>
+        <div class="form-row align-items-center">
+           
+            <div class="col-auto">
+                <label class="sr-only" for="inlineFormInputGroup">Username</label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">任务</div>
+                    </div>
+                    <input type="text" class="form-control" placeholder="请输入任务" style="width: 500px;" v-model.trim="newTaskName" >
+                </div>
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-2" @click="addNewTask">添加新任务</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'TodoInput',
+    props: {
+        list: {
+            type: Array,
+            required: true,
+            default: [],
+        }
+    },
+    emits: ['add'],
+    data() {
+        return {
+            newTaskName: '',
+            nextId: 4,
+        }
+    },
+    methods: {
+        // addNewTask() {
+        //     if (this.newTaskName === '') return alert('输入不能为空')
+        //     this.$emit('add', this.newTaskName)
+        //     this.newTaskName = ''
+        // }
+        addNewTask() {
+            this.list.push({
+                id: this.nextId, task: this.newTaskName, done: false
+            })
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ todo-pro/src/components/todo-list/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="list-group mt-3">
+        <li class="list-group-item d-flex justify-content-between align-items-center" v-for="(item, index) in list"
+            :key="item.id">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" v-model="item.done">
+                <label class="form-check-label" for="defaultCheck1">
+                    {{item.task}}
+                </label>
+            </div>
+            <span class="badge badge-success badge-pill" v-if="item.done">已完成</span>
+            <span class="badge badge-danger badge-pill" v-else>未完成</span>
+        </li>
+
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'TodoList',
+    props: {
+        list: {
+            type: Array,
+            required: true,
+            default: [],
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
